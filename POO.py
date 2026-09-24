85d3000924120ad717654533b17c1e28c8556e49
Abby9602 <aluno01923@gmail.com>
2026-09-24 18:00:28 -0400

    Refactor content classes and instantiation

--- POO.py
+++ POO.py
@@ -1,4 +1,4 @@
-# ===================== CLASSE DAD =====================
+# ===================== CLASSE PRINCIPAL =====================
 class Conteudo:
     def __init__(self, titulo, genero):
         self.titulo = titulo
